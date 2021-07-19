5f5737900d3eda14762bca75d22f4bf675548f74
anapdh <ana_d_hubner@hotmail.com>
2021-07-19 18:25:21 -0300

    Add content in components to diplay in App and delete unused files

--- src/index.js
+++ src/index.js
@@ -2,26 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-// import PropTypes from 'prop-types';
-
-// function HelloWorldComponent({ name }) {
-//   return (
-//     <div>
-//       Hello,
-//       {name}
-//     </div>
-//   );
-// }
-
-// HelloWorldComponent.defaultProps = {
-//   name: 'Ana',
-// };
-
-// HelloWorldComponent.propTypes = {
-//   name: PropTypes.string,
-// };
-
-// export default HelloWorldComponent;
 
 ReactDOM.render(
   <App />,
