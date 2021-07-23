a548f42ecd72750562bd7640154ad57092aee496
anapdh <ana_d_hubner@hotmail.com>
2021-07-23 19:07:57 -0300

    Fix app code

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,41 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <Display result={Calculate.total} />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick(button) {
+    const prev = this.state;
+    this.setState(
+      Calculate(prev, button),
+    );
+  }
+
+  render() {
+    const { total } = this.state;
+    const { next } = this.state;
+    const { operation } = this.state;
+    return (
+      <div className="calculator">
+        <Display
+          result={total}
+          next={next}
+          operation={operation}
+        />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
